3d20f4250c975b95cd830d5c31df3e7ea442bdad
alexander <sasha.los.0148@gmail.com>
2020-08-21 17:51:21 +0600

    add some deps & simple template

---
+++ src/containers/App/components/App.dev.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './App';
+
+const AppDev = () => (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+export default AppDev;
---
+++ src/containers/App/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from 'components/App';
+import AppDev from './components/App.dev';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const Main = () => (isDev ? (<AppDev />) : (<App />));
+
+export default Main;
